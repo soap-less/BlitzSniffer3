1040afd3a666187e11a554db39a7d057748c6f80
OatmealDome <julian@oatmealdome.me>
2020-05-11 14:28:03 -0400

    Initial commit

---
+++ BlitzSniffer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlitzSniffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NintendoNetcode/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NintendoNetcode
+{
+    public class Class1
+    {
+    }
+}
