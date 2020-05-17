786c77eb1106d08098ae26bf26b92c47fe288bd5
OatmealDome <julian@oatmealdome.me>
2020-05-17 16:32:58 -0400

    CloneElementDataEventInitialAck: Fix structure

--- NintendoNetcode/Pia/Clone/Element/Data/Event/CloneElementDataEventInitialAck.cs
+++ NintendoNetcode/Pia/Clone/Element/Data/Event/CloneElementDataEventInitialAck.cs
@@ -5,15 +5,9 @@ namespace NintendoNetcode.Pia.Clone.Element.Data.Event
 {
     class CloneElementDataEventInitialAck : CloneElementDataEvent
     {
-        public ushort Index
-        {
-            get;
-            set;
-        }
-
         public CloneElementDataEventInitialAck(BinaryDataReader reader) : base(reader)
         {
-            reader.Seek(1, SeekOrigin.Begin); // padding
+
         }
 
     }
