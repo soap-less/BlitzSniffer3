9d074b907caf58b017c2c2a63b42a6aba11836cb
OatmealDome <julian@oatmealdome.me>
2020-07-23 01:23:21 -0400

    Resource: Add stage byaml loader

---
+++ BlitzSniffer/Resources/StageResource.cs
@@ -0,0 +1,59 @@
+﻿using Blitz;
+using Nintendo;
+using Nintendo.Archive;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BlitzSniffer.Resources
+{
+    class StageResource
+    {
+        private static StageResource _Instance;
+        
+        public static StageResource Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new StageResource();
+                }
+
+                return _Instance;
+            }
+        }
+
+        private Dictionary<int, string> Stages = new Dictionary<int, string>();
+
+        public StageResource()
+        {
+            dynamic mapInfo;
+            using (Stream mapInfoStream = RomResourceLoader.Instance.GetRomFile("/Mush/MapInfo.release.byml"))
+            {
+                mapInfo = ByamlLoader.LoadByamlDynamic(mapInfoStream);
+            }
+
+            foreach (Dictionary<string, dynamic> map in mapInfo)
+            {
+                int id = map["Id"];
+
+                if (id == -1)
+                {
+                    continue;
+                }
+
+                Stages.Add(id, map["MapFileName"]);
+            }
+        }
+
+        public dynamic LoadStageForId(int id)
+        {
+            using (Stream stream = RomResourceLoader.Instance.GetRomFile($"/Map/{Stages[id]}.szs"))
+            {
+                Sarc sarc = new Sarc(stream);
+                return ByamlLoader.LoadByamlDynamic(sarc[$"{Stages[id]}.byaml"]);
+            }
+        }
+
+    }
+}
