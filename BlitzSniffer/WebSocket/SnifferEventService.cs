77ced800df88fc7de9d652867fe7ce08864163c0
OatmealDome <julian@oatmealdome.me>
2020-07-25 14:52:44 -0400

    Sniffer: Add basic WebSocket server implementation

---
+++ BlitzSniffer/WebSocket/SnifferEventService.cs
@@ -0,0 +1,33 @@
+﻿using BlitzSniffer.Event;
+using System;
+using System.Text.Json;
+using WebSocketSharp.Server;
+
+namespace BlitzSniffer.WebSocket
+{
+    class SnifferEventService : WebSocketBehavior
+    {
+        public SnifferEventService()
+        {
+            EventTracker.Instance.SendEvent += BroadcastEvent;
+        }
+
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+
+            Console.WriteLine("[SnifferEventService] Client connected");
+        }
+
+        private void BroadcastEvent(object sender, SendEventArgs args)
+        {
+            string json = JsonSerializer.Serialize(args.GameEvent, args.GameEvent.GetType(), new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            });
+
+            Sessions.BroadcastAsync(json, null);
+        }
+
+    }
+}
---
+++ BlitzSniffer/WebSocket/SnifferServer.cs
@@ -0,0 +1,33 @@
+﻿using WebSocketSharp.Server;
+
+namespace BlitzSniffer.WebSocket
+{
+    public class SnifferServer
+    {
+        public static SnifferServer Instance = null;
+
+        private WebSocketServer Server
+        {
+            get;
+            set;
+        }
+
+        private SnifferServer()
+        {
+            Server = new WebSocketServer(13370);
+            Server.AddWebSocketService<SnifferEventService>("/Events");
+            Server.Start();
+        }
+
+        public static void Initialize()
+        {
+            if (Instance != null)
+            {
+                return;
+            }
+
+            Instance = new SnifferServer();
+        }
+
+    }
+}
