f80dd919e63de7fdd1843e800dcab4d6b5561c47
OatmealDome <julian@oatmealdome.me>
2020-07-24 23:31:32 -0400

    Sniffer: Add EnlHolder to assist with handling system record changes

---
+++ BlitzSniffer/Enl/EnlHolder.cs
@@ -0,0 +1,44 @@
+﻿using NintendoNetcode.Enl;
+using NintendoNetcode.Enl.Record;
+
+namespace BlitzSniffer.Enl
+{
+    public class EnlHolder
+    {
+        private static EnlHolder _Instance = null;
+
+        public static EnlHolder Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new EnlHolder();
+                }
+
+                return _Instance;
+            }
+        }
+
+        public delegate void SystemInfoReceivedEventHandler(object sender, SystemInfoReceivedEventArgs args);
+        public event SystemInfoReceivedEventHandler SystemInfoReceived;
+
+        public EnlHolder()
+        {
+
+        }
+
+        public void EnlMessageReceived(EnlMessage message)
+        {
+            foreach (EnlRecord record in message.Records)
+            {
+                if (record is EnlSystemInfoRecord)
+                {
+                    EnlSystemInfoRecord systemInfoRecord = record as EnlSystemInfoRecord;
+                    SystemInfoReceived(this, new SystemInfoReceivedEventArgs(systemInfoRecord));
+                }
+            }
+        }
+
+    }
+}
---
+++ BlitzSniffer/Enl/SystemInfoReceivedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using NintendoNetcode.Enl.Record;
+using System;
+
+namespace BlitzSniffer.Enl
+{
+    public class SystemInfoReceivedEventArgs : EventArgs
+    {
+        public EnlSystemInfoRecord Record
+        {
+            get;
+            private set;
+        }
+
+        public SystemInfoReceivedEventArgs(EnlSystemInfoRecord record)
+        {
+            Record = record;
+        }
+
+    }
+}
