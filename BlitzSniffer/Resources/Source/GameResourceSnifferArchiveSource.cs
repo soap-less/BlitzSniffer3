3d955aaf620840737661cb4d0f3f12526d403400
OatmealDome <julian@oatmealdome.me>
2020-07-26 15:51:25 -0400

    Resource: Add support for loading ROM files from a specially built SARC

---
+++ BlitzSniffer/Resources/Source/GameResourceSnifferArchiveSource.cs
@@ -0,0 +1,29 @@
+﻿using Nintendo.Archive;
+using System.IO;
+
+namespace BlitzSniffer.Resources.Source
+{
+    class GameResourceSnifferArchiveSource : GameResourceSource
+    {
+        private readonly static string ArchivePath = "GameData.sarc";
+
+        private Sarc SnifferArchive;
+
+        private GameResourceSnifferArchiveSource()
+        {
+            SnifferArchive = new Sarc(File.OpenRead(ArchivePath));
+        }
+
+        public static void Initialize()
+        {
+            Instance = new GameResourceSnifferArchiveSource();
+        }
+
+        protected override Stream GetFileInternal(string path)
+        {
+            byte[] rawFile = SnifferArchive[path.StartsWith('/') ? path.Substring(1) : path];
+            return new MemoryStream(rawFile);
+        }
+
+    }
+}
