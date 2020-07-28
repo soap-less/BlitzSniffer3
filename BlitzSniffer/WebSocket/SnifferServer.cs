66cde745c5ca9de56b6cdbe3b38043caa5198214
OatmealDome <julian@oatmealdome.me>
2020-07-28 03:29:33 -0400

    SnifferServer: Redirect WebSocketSharp's logger to Serilog

--- BlitzSniffer/WebSocket/SnifferServer.cs
+++ BlitzSniffer/WebSocket/SnifferServer.cs
@@ -1,9 +1,16 @@
-﻿using WebSocketSharp.Server;
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.Reflection;
+using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace BlitzSniffer.WebSocket
 {
     public class SnifferServer
     {
+        private static readonly ILogger WebSocketSharpLogContext = Log.ForContext(Constants.SourceContextPropertyName, "WebSocketSharp");
+
         public static SnifferServer Instance = null;
 
         private WebSocketServer Server
@@ -15,6 +22,17 @@ namespace BlitzSniffer.WebSocket
         private SnifferServer()
         {
             Server = new WebSocketServer(13370);
+
+            // Get the WebSocketServer's logger
+            FieldInfo loggerField = Server.GetType().GetField("_log", BindingFlags.NonPublic | BindingFlags.Instance);
+            object webSocketLogger = loggerField.GetValue(Server);
+
+            // Override the output mechanism to our own which prints to Serilog
+            Type webSocketLoggerType = webSocketLogger.GetType();
+            FieldInfo actionField = webSocketLoggerType.GetField("_output", BindingFlags.NonPublic | BindingFlags.Instance);
+            Action<LogData, string> outputAction = (LogData logData, string path) => LogToSerilog(logData, path);
+            actionField.SetValue(webSocketLogger, outputAction);
+
             Server.AddWebSocketService<SnifferEventService>("/Events");
             Server.Start();
         }
@@ -27,6 +45,31 @@ namespace BlitzSniffer.WebSocket
             }
 
             Instance = new SnifferServer();
+        }
+
+        private void LogToSerilog(LogData logData, string path)
+        {
+            switch (logData.Level)
+            {
+                case LogLevel.Trace:
+                    WebSocketSharpLogContext.Verbose(logData.Message);
+                    break;
+                case LogLevel.Debug:
+                    WebSocketSharpLogContext.Debug(logData.Message);
+                    break;
+                case LogLevel.Info:
+                    WebSocketSharpLogContext.Information(logData.Message);
+                    break;
+                case LogLevel.Warn:
+                    WebSocketSharpLogContext.Warning(logData.Message);
+                    break;
+                case LogLevel.Error:
+                    WebSocketSharpLogContext.Error(logData.Message);
+                    break;
+                case LogLevel.Fatal:
+                    WebSocketSharpLogContext.Fatal(logData.Message);
+                    break;
+            }
         }
 
     }
