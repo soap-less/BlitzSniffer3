b99bba797d39742dbf1a51114b110deb07c0f2d7
OatmealDome <julian@oatmealdome.me>
2020-07-29 14:32:09 -0400

    SnifferEventService: Use Serilog instead of Console

--- BlitzSniffer/WebSocket/SnifferEventService.cs
+++ BlitzSniffer/WebSocket/SnifferEventService.cs
@@ -1,5 +1,6 @@
 ﻿using BlitzSniffer.Event;
-using System;
+using Serilog;
+using Serilog.Core;
 using System.Text.Json;
 using WebSocketSharp.Server;
 
@@ -7,6 +8,8 @@ namespace BlitzSniffer.WebSocket
 {
     class SnifferEventService : WebSocketBehavior
     {
+        private static readonly ILogger LogContext = Serilog.Log.ForContext(Constants.SourceContextPropertyName, "SnifferEventService");
+
         public SnifferEventService()
         {
             EventTracker.Instance.SendEvent += BroadcastEvent;
@@ -16,7 +19,7 @@ namespace BlitzSniffer.WebSocket
         {
             base.OnOpen();
 
-            Console.WriteLine("[SnifferEventService] Client connected");
+            LogContext.Information("Client connected");
         }
 
         private void BroadcastEvent(object sender, SendEventArgs args)
