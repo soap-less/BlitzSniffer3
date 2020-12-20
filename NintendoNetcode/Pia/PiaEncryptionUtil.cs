6ae3cb5139aaf78c7225d19b9408051fb33372e3
OatmealDome <julian@oatmealdome.me>
2020-12-19 23:21:19 -0500

    PiaEncryptionUtil: Add method to generate a LAN crypto challenge

--- NintendoNetcode/Pia/PiaEncryptionUtil.cs
+++ NintendoNetcode/Pia/PiaEncryptionUtil.cs
@@ -1,4 +1,6 @@
+﻿using NintendoNetcode.Pia.Lan.Content.Browse.Crypto;
 using System;
+using System.IO;
 using System.Security.Cryptography;
 
 namespace NintendoNetcode.Pia
@@ -21,6 +23,67 @@ namespace NintendoNetcode.Pia
             Array.Copy(hmac, sessionKey, 16);
 
             return sessionKey;
+        }
+
+        public static LanCryptoContentChallenge GenerateLanCryptoChallenge(byte[] broadcastAddress, long counter, byte[] gameKey)
+        {
+            byte[] counterArray = BitConverter.GetBytes(counter);
+
+            if (BitConverter.IsLittleEndian)
+            {
+                // Convert to network order
+                // Array.Reverse(broadcastAddress); // already in network order
+                Array.Reverse(counterArray);
+            }
+
+            byte[] nonce = new byte[12];
+            Array.Copy(broadcastAddress, nonce, 4);
+            Array.Copy(counterArray, 0, nonce, 4, 8);
+
+            RNGCryptoServiceProvider secureRandom = new RNGCryptoServiceProvider();
+
+            byte[] challengeKey = new byte[16];
+            secureRandom.GetBytes(challengeKey);
+
+            byte[] challengeData = new byte[256];
+            secureRandom.GetBytes(challengeData);
+
+            byte[] encryptionKey;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Mode = CipherMode.ECB;
+                aes.Padding = PaddingMode.None;
+                aes.Key = gameKey;
+                aes.IV = new byte[16]; // empty IV for ECB
+
+                using (MemoryStream encryptedStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(encryptedStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(challengeKey);
+                    }
+
+                    encryptionKey = encryptedStream.ToArray();
+                }
+            }
+
+            byte[] ciphertext = new byte[256];
+            byte[] tag = new byte[16];
+
+            using (AesGcm aesGcm = new AesGcm(encryptionKey))
+            {
+                aesGcm.Encrypt(nonce, challengeData, ciphertext, tag);
+            }
+
+            return new LanCryptoContentChallenge()
+            {
+                Version = 1, // old InetAddress format without IPv6 support
+                CryptoEnabled = true,
+                Counter = counter,
+                ChallengeKey = challengeKey,
+                Tag = tag,
+                Data = ciphertext
+            };
         }
 
     }
