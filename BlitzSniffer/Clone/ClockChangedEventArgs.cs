6e00b4497e33f0f121f328ea94b384e9210173b5
OatmealDome <julian@oatmealdome.me>
2020-12-25 14:05:51 -0500

    Clone: Add ClockChangedEventArgs

---
+++ BlitzSniffer/Clone/ClockChangedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BlitzSniffer.Clone
+{
+    public class ClockChangedEventArgs : EventArgs
+    {
+        public uint Clock
+        {
+            get;
+            set;
+        }
+
+        public ClockChangedEventArgs(uint clock)
+        {
+            Clock = clock;
+        }
+
+    }
+}
