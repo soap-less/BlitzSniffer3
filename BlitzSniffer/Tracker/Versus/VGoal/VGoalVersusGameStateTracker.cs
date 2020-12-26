e1ccaefd5d930c424d9f4518473bffa921461e30
OatmealDome <julian@oatmealdome.me>
2020-12-26 00:48:05 -0500

    VGoalVersusGameStateTracker: Implement reading Gachihoko state

--- BlitzSniffer/Tracker/Versus/VGoal/VGoalVersusGameStateTracker.cs
+++ BlitzSniffer/Tracker/Versus/VGoal/VGoalVersusGameStateTracker.cs
@@ -1,7 +1,11 @@
-﻿using Blitz.Cmn.Def;
+using Blitz.Cmn.Def;
 using BlitzSniffer.Clone;
+using BlitzSniffer.Event;
+using BlitzSniffer.Event.Versus;
+using BlitzSniffer.Util;
 using Nintendo.Sead;
 using Syroot.BinaryData;
+using System.IO;
 
 namespace BlitzSniffer.Tracker.Versus.VGoal
 {
@@ -9,8 +13,12 @@ namespace BlitzSniffer.Tracker.Versus.VGoal
     {
         public override VersusRule Rule => VersusRule.Vgl;
 
+        private uint GachihokoTimeout;
+
         public VGoalVersusGameStateTracker(ushort stage, Color4f alpha, Color4f bravo) : base(stage, alpha, bravo)
         {
+            GachihokoTimeout = 0;
+
             CloneHolder holder = CloneHolder.Instance;
             holder.RegisterClone(121);
             holder.CloneChanged += HandleGachihokoState;
@@ -29,6 +37,44 @@ namespace BlitzSniffer.Tracker.Versus.VGoal
             if (args.CloneId != 121 || args.ElementId != 2)
             {
                 return;
+            }
+
+            using (MemoryStream stream = new MemoryStream(args.Data))
+            using (BinaryDataReader reader = new BinaryDataReader(stream))
+            {
+                reader.ByteOrder = ByteOrder.LittleEndian;
+
+                BitReader bitReader = new BitReader(reader);
+
+                uint hokoTimeout = bitReader.ReadVariableBits(16);
+
+                if (hokoTimeout != GachihokoTimeout)
+                {
+                    GachihokoTimeout = hokoTimeout;
+                }
+
+                uint ReadScore()
+                {
+                    return (uint)((bitReader.ReadF32Bit(2, 14) - 1.0f) * 100.0f);
+                }
+
+                uint bravoBestScore = ReadScore(); // yes, bravo first
+                uint alphaBestScore = ReadScore();
+
+                if (alphaBestScore != AlphaScore || bravoBestScore != BravoScore)
+                {
+                    AlphaScore = alphaBestScore;
+                    BravoScore = bravoBestScore;
+
+                    EventTracker.Instance.AddEvent(new GachiScoreUpdateEvent()
+                    {
+                        AlphaScore = AlphaScore,
+                        BravoScore = BravoScore
+                    });
+                }
+
+                // Current position
+                // float position = bitReader.ReadF32Bit(3, 14) - 2.0f;
             }
         }
 
