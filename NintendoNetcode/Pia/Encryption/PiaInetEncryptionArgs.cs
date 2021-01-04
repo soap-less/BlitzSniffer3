70706b92a29e500ff8ec5b95aa580314e08c682a
OatmealDome <julian@oatmealdome.me>
2021-01-04 16:33:02 -0500

    PiaInetEncryptionArgs: Add args for Inet sessions

---
+++ NintendoNetcode/Pia/Encryption/PiaInetEncryptionArgs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NintendoNetcode.Pia.Encryption
+{
+    public class PiaInetEncryptionArgs : PiaEncryptionArgs
+    {
+        private uint GatheringId;
+
+        public PiaInetEncryptionArgs(byte[] key, uint gatheringid) : base(key)
+        {
+            GatheringId = gatheringid;
+        }
+
+        public override byte[] GetNonce(byte connectionId, byte[] headerNonce)
+        {
+            byte[] nonce = new byte[12];
+
+            nonce[0] = connectionId;
+
+            nonce[1] = (byte)((GatheringId >> 16) & 0xFF);
+            nonce[2] = (byte)((GatheringId >> 8) & 0xFF);
+            nonce[3] = (byte)(GatheringId & 0xFF);
+
+            Array.Copy(headerNonce, 0, nonce, 4, 8);
+
+            return nonce;
+        }
+
+    }
+}
