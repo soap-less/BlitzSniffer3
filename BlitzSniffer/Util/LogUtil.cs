e6308dd31e51ceea6bb789b7d15c9d445c174bfb
OatmealDome <julian@oatmealdome.me>
2021-01-08 04:32:41 -0500

    LogUtil: Add utility method for creating an ILogger

---
+++ BlitzSniffer/Util/LogUtil.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace BlitzSniffer.Util
+{
+    static class LogUtil
+    {
+        public static ILogger GetLogger(string name)
+        {
+            return Log.ForContext(Constants.SourceContextPropertyName, name);
+        }
+
+    }
+}
