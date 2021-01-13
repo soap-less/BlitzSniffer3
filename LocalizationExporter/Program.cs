65927db2ae09dcfa56dccfab8284a0f3493f73c9
OatmealDome <julian@oatmealdome.me>
2021-01-13 03:41:54 -0500

    LocalizationExporter: Initialize Serilog

--- LocalizationExporter/Program.cs
+++ LocalizationExporter/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Serilog;
+using Serilog.Core;
 
 namespace LocalizationExporter
 {
     class Program
     {
+        private static readonly string LOG_FORMAT = "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: LOG_FORMAT)
+                .CreateLogger();
+
+            ILogger logContext = Log.ForContext(Constants.SourceContextPropertyName, "Program");
+
+            logContext.Information("LocalizationExporter {Version}", ThisAssembly.AssemblyFileVersion);
         }
 
     }
