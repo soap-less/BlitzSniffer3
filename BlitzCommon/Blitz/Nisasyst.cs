207cb9fd07198784978e47feacca023e64f95d10
OatmealDome <julian@oatmealdome.me>
2021-01-13 03:28:01 -0500

    Crc32: Move to BlitzCommon.Util namespace

--- BlitzCommon/Blitz/Nisasyst.cs
+++ BlitzCommon/Blitz/Nisasyst.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using BlitzCommon.Util;
+using System;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using Util;
 
 namespace Blitz
 {
