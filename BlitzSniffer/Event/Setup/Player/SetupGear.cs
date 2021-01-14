95f63e213e1bd9929210ebdfb1355b2b20b91be8
OatmealDome <julian@oatmealdome.me>
2021-01-14 04:55:30 -0500

    SetupGear: Add kind prefix to Id

--- BlitzSniffer/Event/Setup/Player/SetupGear.cs
+++ BlitzSniffer/Event/Setup/Player/SetupGear.cs
@@ -38,7 +38,26 @@ namespace BlitzSniffer.Event.Setup.Player
 
         public SetupGear(GearKind kind, Gear gear)
         {
-            Id = GearResource.Instance.GetGear(kind, (int)gear.Id);
+            string name = GearResource.Instance.GetGear(kind, (int)gear.Id);
+
+            string kindStr;
+            switch (kind)
+            {
+                case GearKind.Head:
+                    kindStr = "Hed";
+                    break;
+                case GearKind.Clothes:
+                    kindStr = "Clt";
+                    break;
+                case GearKind.Shoes:
+                    kindStr = "Shs";
+                    break;
+                default:
+                    kindStr = "Unk";
+                    break;
+            }
+
+            Id = $"{kindStr}_{name}";
             Main = Enum.GetName(typeof(GearSkill), gear.MainSkill);
             SecondaryOne = Enum.GetName(typeof(GearSkill), gear.SecondarySkillOne);
             SecondaryTwo = Enum.GetName(typeof(GearSkill), gear.SecondarySkillTwo);
