3f8e0ae95e49d677e563e214cb55d74225c1baaf
OatmealDome <julian@oatmealdome.me>
2021-01-17 23:50:19 -0500

    SnifferResourceEncryptor: Add program to encrypt resources

---
+++ SnifferResourceEncryptor/Program.cs
@@ -0,0 +1,40 @@
+﻿using BlitzCommon.Resources;
+using Serilog;
+using Serilog.Core;
+using System;
+using System.IO;
+
+namespace SnifferResourceEncryptor
+{
+    class Program
+    {
+        private static readonly string LOG_FORMAT = "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+        static void Main(string targetFile)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(outputTemplate: LOG_FORMAT)
+                .CreateLogger();
+
+            ILogger logContext = Log.ForContext(Constants.SourceContextPropertyName, "Program");
+
+            logContext.Information("SnifferResourceEncryptor {Version}", ThisAssembly.AssemblyFileVersion);
+
+            byte[] data = File.ReadAllBytes(targetFile);
+
+            logContext.Information("Read file");
+
+            SnifferResource resource = new SnifferResource(data);
+            byte[] file = resource.Serialize();
+
+            logContext.Information("Encryption successful");
+
+            logContext.Information("Key: {Key}", BitConverter.ToString(resource.Key).Replace("-", "").ToLower());
+
+            File.WriteAllBytes(targetFile + "-enc", file);
+
+            logContext.Information("Done!");
+        }
+    }
+}
